e90ab7d133ea1d963de9845b9606f04141abaefd
sam <40480888+Sam-Hill69@users.noreply.github.com>
2025-03-20 21:47:43 -0500

    started my hw assignment

---
+++ java/class_Assignments/prjClasses_Hill.java
@@ -0,0 +1,13 @@
+// Once you have written the class, write a separate program that
+// creates three employee objects to hold the following data:
+// (see book for data)
+// The program should store this data in the three objects and then display the data for each employee on the screen.
+
+public class prjClasses_Hill 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+    
+}

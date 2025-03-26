fbc5230d57ce7459029149a528ad7ae0f1bfadf9
sam <40480888+Sam-Hill69@users.noreply.github.com>
2025-03-26 13:16:00 -0500

    Created a program to calculate different parts of a circle

---
+++ java/class_Assignments/OOP/Circle.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Circle
+{
+    private Double radius;
+    final Double PI = 3.14159;
+
+    public static void main(String[] args) 
+    {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What is the circles radius? ");
+        Double radius = keyboard.nextDouble();
+        Circle c = new Circle(radius);
+        System.out.println("The radius of your circle is: " + c.getRadius());
+        System.out.println("The area of your circle is: " + c.area());
+        System.out.println("The diameter of your circle is: " + c.diameter());
+        System.out.println("The Circumfrence of your circle is: " + c.circumfrence());
+    }
+    
+
+    public Circle(Double radius)
+    {
+        this.radius = radius;
+    }
+
+    public Circle()
+    {
+        this.radius = 0.0;
+    }
+
+    public void setRadius(Double radius)
+    {
+        this.radius = radius;
+    }
+
+    public Double getRadius()
+    {
+        return radius;
+    }
+
+    public Double area()
+    {
+        return PI * radius * radius;
+    }
+
+    public Double diameter()
+    {
+        return radius * 2;
+    }
+
+    public Double circumfrence()
+    {
+        return 2 * PI * radius;
+    }
+}

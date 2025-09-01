7cd66707bbee29fcc2abcbf28cf6a1ab38793bae
Sam Hill <40480888+Sam-Hill69@users.noreply.github.com>
2025-09-01 18:53:02 -0500

    added a method to pull the arraylist values, orginized by 10

--- java/class_Assignments/Assignment1/Assignment1.java
+++ java/class_Assignments/Assignment1/Assignment1.java
@@ -1,22 +1,112 @@
 import java.util.*;
 
-public class Assignment1 {
-
-    public static void main(String[] args) {
-        
+public class Assignment1 
+{
+    public static void main(String[] args) 
+    {
         // creates an array named randomIntergers and stores 100 elements
-        int[] randomNumbers = new int[100];
+        ArrayList<Integer> randomNumbers = new ArrayList<Integer>(99);
 
-        // creates the random number
+        // creates the random number generator
         Random rand = new Random();
 
-        // stores random intergers in the array
-        for (int i=0; i<randomNumbers.length; i++)
+        // stores random integers in the array
+        for (int i = 0; i < 100; i++) 
         {
-            randomNumbers[i] = rand.nextInt(100);
+            randomNumbers.add(rand.nextInt(1000));
         }
 
-
+        displayArray(randomNumbers);
     }
-    
+
+    public static void displayArray(ArrayList<Integer> randomNumbers)
+    {
+        for (int i = 0; i < 99; i++)
+        {
+            if (i == 0)
+            {
+                System.out.print("\n" + randomNumbers.get(i) + " ");
+            }
+            else if (i > 0 && i < 10)
+            {
+                System.out.print(randomNumbers.get(i) + " ");
+            }
+            else if (i == 10)
+            {
+                System.out.print("\n" + randomNumbers.get(i) + " ");
+            }
+            else if (i > 10 && i < 20)
+            {
+                System.out.print(randomNumbers.get(i) + " ");
+            }
+            else if (i == 20)
+            {
+                System.out.print("\n" + randomNumbers.get(i) + " ");
+            }
+            else if (i > 20 && i < 30)
+            {
+                System.out.print(randomNumbers.get(i) + " ");
+            }
+            else if (i == 30)
+            {
+                System.out.print("\n" + randomNumbers.get(i) + " ");
+            }
+            else if (i > 30 && i < 40)
+            {
+                System.out.print(randomNumbers.get(i) + " ");
+            }
+            else if (i == 40)
+            {
+                System.out.print("\n" + randomNumbers.get(i) + " ");
+            }
+            else if (i > 40 && i < 50)
+            {
+                System.out.print(randomNumbers.get(i) + " ");
+            }
+            else if (i == 50)
+            {
+                System.out.print("\n" + randomNumbers.get(i) + " ");
+            }
+            else if (i > 50 && i < 60)
+            {
+                System.out.print(randomNumbers.get(i) + " ");
+            }
+            else if (i == 60)
+            {
+                System.out.print("\n" + randomNumbers.get(i) + " ");
+            }
+            else if (i > 60 && i < 70)
+            {
+                System.out.print(randomNumbers.get(i) + " ");
+            }
+            else if (i == 70)
+            {
+                System.out.print("\n" + randomNumbers.get(i) + " ");
+            }
+            else if (i > 70 && i < 80)
+            {
+                System.out.print(randomNumbers.get(i) + " ");
+            }
+            else if (i == 80)
+            {
+                System.out.print("\n" + randomNumbers.get(i) + " ");
+            }
+            else if (i > 80 && i < 90)
+            {
+                System.out.print(randomNumbers.get(i) + " ");
+            }
+            else if (i == 90)
+            {
+                System.out.print("\n" + randomNumbers.get(i) + " ");
+            }
+            else if (i > 90 && i < 99)
+            {
+                System.out.print(randomNumbers.get(i) + " ");
+            }
+        }
+            
+    }
+        
+
 }
+

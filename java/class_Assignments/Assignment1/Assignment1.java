f3437db4aec74decd78baa285432c83f4aebae56
Sam Hill <40480888+Sam-Hill69@users.noreply.github.com>
2025-09-03 05:30:55 -0500

    refactored the method that orginizes the list by 10

--- java/class_Assignments/Assignment1/Assignment1.java
+++ java/class_Assignments/Assignment1/Assignment1.java
@@ -23,83 +23,47 @@ public class Assignment1
     {
         for (int i = 0; i < 99; i++)
         {
-            if (i == 0)
+            if (i == 0 )
             {
                 System.out.print("\n" + randomNumbers.get(i) + " ");
-            }
-            else if (i > 0 && i < 10)
-            {
-                System.out.print(randomNumbers.get(i) + " ");
             }
             else if (i == 10)
             {
                 System.out.print("\n" + randomNumbers.get(i) + " ");
             }
-            else if (i > 10 && i < 20)
-            {
-                System.out.print(randomNumbers.get(i) + " ");
-            }
             else if (i == 20)
             {
                 System.out.print("\n" + randomNumbers.get(i) + " ");
-            }
-            else if (i > 20 && i < 30)
-            {
-                System.out.print(randomNumbers.get(i) + " ");
             }
             else if (i == 30)
             {
                 System.out.print("\n" + randomNumbers.get(i) + " ");
             }
-            else if (i > 30 && i < 40)
-            {
-                System.out.print(randomNumbers.get(i) + " ");
-            }
             else if (i == 40)
             {
                 System.out.print("\n" + randomNumbers.get(i) + " ");
-            }
-            else if (i > 40 && i < 50)
-            {
-                System.out.print(randomNumbers.get(i) + " ");
             }
             else if (i == 50)
             {
                 System.out.print("\n" + randomNumbers.get(i) + " ");
             }
-            else if (i > 50 && i < 60)
-            {
-                System.out.print(randomNumbers.get(i) + " ");
-            }
             else if (i == 60)
             {
                 System.out.print("\n" + randomNumbers.get(i) + " ");
-            }
-            else if (i > 60 && i < 70)
-            {
-                System.out.print(randomNumbers.get(i) + " ");
             }
             else if (i == 70)
             {
                 System.out.print("\n" + randomNumbers.get(i) + " ");
             }
-            else if (i > 70 && i < 80)
-            {
-                System.out.print(randomNumbers.get(i) + " ");
-            }
             else if (i == 80)
             {
                 System.out.print("\n" + randomNumbers.get(i) + " ");
-            }
-            else if (i > 80 && i < 90)
-            {
-                System.out.print(randomNumbers.get(i) + " ");
             }
             else if (i == 90)
             {
                 System.out.print("\n" + randomNumbers.get(i) + " ");
             }
-            else if (i > 90 && i < 99)
+            else if (i < 99)
             {
                 System.out.print(randomNumbers.get(i) + " ");
             }
